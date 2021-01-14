d386260e57c10f9886c783ef5c2a86fb080cd4ca
Yuriy Martini <yuriy.martini@live.it>
2021-01-14 22:26:24 +0100

    refactoring

--- resources/views/vendor/nova/auth/partials/errors.blade.php
+++ resources/views/vendor/nova/auth/partials/errors.blade.php
@@ -1,11 +1,11 @@
 @if ($errors->any())
-<p class="text-center font-semibold text-danger my-3">
-    @if ($errors->has('email'))
-        {{ $errors->first('email') }}
-    @elseif($errors->has('password'))
-        {{ $errors->first('password') }}
-    @else
-        {{ $errors->first('g-recaptcha-response') }}
-    @endif
+    <p class="text-center font-semibold text-danger my-3">
+        @if ($errors->has('email'))
+            {{ $errors->first('email') }}
+        @elseif($errors->has('password'))
+            {{ $errors->first('password') }}
+        @elseif($errors->has('g-recaptcha-response'))
+            {{ $errors->first('g-recaptcha-response') }}
+        @endif
     </p>
 @endif
